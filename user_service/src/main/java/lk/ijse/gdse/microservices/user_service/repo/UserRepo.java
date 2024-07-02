2223af5b70a3a21822f8f55469b43b9594d1baad
MSI <chathurabhashitha01@gmail.com>
2024-07-03 01:01:14 +0530

    user dto creation

--- user_service/src/main/java/lk/ijse/gdse/microservices/user_service/repo/UserRepo.java
+++ user_service/src/main/java/lk/ijse/gdse/microservices/user_service/repo/UserRepo.java
@@ -3,5 +3,5 @@ package lk.ijse.gdse.microservices.user_service.repo;
 import lk.ijse.gdse.microservices.user_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Integer> {
+public interface UserRepo extends JpaRepository<User, String> {
 }
--- user_service/src/main/java/lk/ijse/gdse/microservices/user_service/service/impl/UserServiceImpl.java
+++ user_service/src/main/java/lk/ijse/gdse/microservices/user_service/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.microservices.user_service.service.impl;
 
+import jakarta.ws.rs.NotFoundException;
 import lk.ijse.gdse.microservices.user_service.dto.UserDTO;
 import lk.ijse.gdse.microservices.user_service.entity.User;
 import lk.ijse.gdse.microservices.user_service.repo.UserRepo;
@@ -27,11 +28,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void registerUser(UserDTO userDTO) {
+        if(userRepo.existsById(userDTO.getUser_NIC())){
+            throw new RuntimeException("Customer Id "+userDTO.getUser_NIC()+" All ready exist");
+        }
         userRepo.save(modelMapper.map(userDTO, User.class));
     }
 
     @Override
     public void updateUser(UserDTO userDTO) {
+        if(!userRepo.existsById(userDTO.getUser_NIC())){
+            throw new RuntimeException("Customer Id "+userDTO.getUser_NIC()+" does not exist");
+        }
         userRepo.save(modelMapper.map(userDTO, User.class));
     }
 }
