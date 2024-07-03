2a8903dc3f47ff87214a94f302c9d8785a5ef1ce
MSI <chathurabhashitha01@gmail.com>
2024-07-03 23:41:15 +0530

    ticket validation  creation

--- ticket_service/src/main/java/lk/ijse/gdse/microservices/ticket_service/controller/TicketController.java
+++ ticket_service/src/main/java/lk/ijse/gdse/microservices/ticket_service/controller/TicketController.java
@@ -5,6 +5,7 @@ import lk.ijse.gdse.microservices.ticket_service.dto.TicketDTO;
 import lk.ijse.gdse.microservices.ticket_service.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,17 +18,17 @@ public class TicketController {
     @Autowired
     private TicketService ticketService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<TicketDTO> getTicketsDetails() {
         return ticketService.getTickets();
     }
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void saveTicketDetails(@RequestBody TicketDTO ticketDTO) {
         ticketService.addTicket(ticketDTO);
     }
 
-    @PutMapping("/updateStatus")
+    @PutMapping(value = "/updateStatus",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateTicketDetails(@RequestBody PaymentDTO paymentDTO) {
         System.out.println(paymentDTO);
         ticketService.updateTicket(paymentDTO);
